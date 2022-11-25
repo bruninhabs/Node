ac179845d5d64f38598b9029b1c736aebf4850b2
bruninhabs <bruninhabs962@gmail.com>
2022-11-25 10:33:25 -0300

    data hora e placar

--- aula1.js
+++ aula1.js
@@ -1,6 +1,8 @@
 var http = require('http');
 http.createServer(function (req,res){
-    res.write('Brasil 3 x 1 Servia \n');
+    res.writeHead(200,{'Content-Type':'test/html'});
+    res.write('<h2> Brasil 3 x 1 Servia </h2>\n ');
+   res.write(Date().substring(16,24)+ '<br>');
     res.end('Leo viado');
 
 }).listen(8080)
