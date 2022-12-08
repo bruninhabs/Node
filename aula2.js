0cf4feb3b5e7342f8da2d556216e34d1c2010b1a
bruninhabs <bruninhabs962@gmail.com>
2022-12-08 08:20:15 -0300

    versao 2

--- aula2.js
+++ aula2.js
@@ -1,8 +1,9 @@
 var http = require('http');
 http.createServer(function (req,res){
     res.writeHead(200,{'Content-Type':'test/html'});
-    res.write('<h2> Brasil 3 x 1 Servia </h2>\n ');
+    res.write('<h2> Brasil 2 x 1 Suica </h2>\n ');
    res.write(Date().substring(16,24)+ '<br>');
+   res.write(dia.diaDaSemana()+'</p><br>')
     res.end('Leo viado');
 
 }).listen(8080)
